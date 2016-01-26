8d562eb2f7c4e97e821b136cd4c6f75935ed1172
Dalton Wu <dalton.z.wu@gmail.com>
2016-01-25 19:22:55 -0500

    Update

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *getBoard(int n) {
+	/**
+	 * Gets the nth line of the boards file.
+	 */
+}
+
 int main() {
 	FILE *fp = fopen("subig20.txt", "r");
 	char *board = NULL;
 	size_t len = 0;
+	
 	if(fp == NULL) {
 		fprintf(stderr, "File not found: subig20.txt\n");
 		exit(1);
@@ -12,4 +19,5 @@ int main() {
 	
 	getline(&board, &len, fp);
 	printf("%s\n", board);
+	return 0;
 }
